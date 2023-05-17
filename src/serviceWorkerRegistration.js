13bea85cb12895b53f9e242814f85a0eb2da1e9a
Laszlo-VSC <laszloluque@gmail.com>
2023-05-17 16:31:10 +0200

    CORS fix

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -11,7 +11,7 @@
 // opt-in, read https://cra.link/PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'https://football-hub-v2.netlify.app/' ||
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
