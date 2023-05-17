9fe9718843cb648f6addc5cb9f05507e82487023
Laszlo-VSC <laszloluque@gmail.com>
2023-05-17 19:32:23 +0200

    cors fix attempt

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -60,7 +60,8 @@ const Home = ({ username, favouriteTeam, favouriteLeague, favouriteFixtures }) =
                 try{
                     //Fetches the data from the API where i is the API in apiKeys' array
                     const getTodaysMatches = await axios.get(mapAPIs[i].link + "matches",
-                    { headers: { "X-Auth-Token": mapAPIs[i].token, "Access-Control-Allow-Origin": "https://football-hub-v2.netlify.app/"  } });
+                    { headers: { "X-Auth-Token": mapAPIs[i].token, 'Access-Control-Allow-Origin' : '*',
+                    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',  } });
                     //If the status of the request is ok it stores matches in useState, stops the loop, and displays the data in the webpage
                     if(getTodaysMatches.status ===  200){
                         setTodaysMatches(getTodaysMatches);
